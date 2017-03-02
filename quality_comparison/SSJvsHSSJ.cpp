744cc6a24e563d16f5b9580f3877ec0fd3d24961
Abe Wits <abewits@gmail.com>
2017-03-02 17:32:51 +0100

    write WS-join and HWS-join

--- quality_comparison/SSJvsHSSJ.cpp
+++ quality_comparison/SSJvsHSSJ.cpp
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <algorithm>
 #include <numeric>
+#include <functional>
 #include "sampleJoins.h"
 
 using namespace std;
@@ -34,8 +35,56 @@ vector<double> get_distribution(int n, double skew, double ratio = 0.0, double n
     return w;    
 }
 
-double aggregate_f(double A, double B, double C) {
-    return A+B*C;
+
+//Runs in O(n_1 log n_1+n_2 log n_2)
+//Output probability is h1*h2
+double generic_sample_join(function<double(double,double)> h1, function<double(double)> h2, int m,
+                                const vector<pdd>& R1, const vector<pdd>& R2,
+                                function<vector<int>(int, vector<double>)> range_sampler,
+                                            //sample(sample_size, weights)
+                                function<double(double, double, double)> aggregation_f) {
+    Tstrat R2_stratified = stratify(R2);
+    map<double, double> R2_stratum_weights;
+    map<double, vector<double> > R2_stratum_cdfs;
+    for(auto stratum : R2_stratified) {
+        double key = stratum.first;
+        double norm = 0.0;
+        vector<double> stratum_weights(stratum.second.size());
+        for(int i=0; i<stratum.second.size(); i++) {
+            pdd t2 = stratum.second[i];
+            stratum_weights[i] = h2(t2.second);
+            norm += stratum_weights[i];
+        }
+        R2_stratum_weights[key] = norm;
+        R2_stratum_cdfs[key] = get_cdf(stratum_weights);
+    }
+    double normalisation = 0.0;
+    vector<double> R1_sample_weights(R1.size());
+    for(int i=0; i<R1.size(); i++) {
+        pdd t1 = R1[i];
+        R1_sample_weights[i] = h1(t1.first, t1.second) * R2_stratum_weights[t1.first];
+        normalisation += R1_sample_weights[i];
+    }
+    
+    vector<int> S_indices = range_sampler(m, R1_sample_weights);
+    vector<pdd> S(m);
+    vector<double> S_weights(m);
+    for(int i=0; i<m; i++) {
+        S[i] = R1[S_indices[i]];
+        S_weights[i] = R1_sample_weights[S_indices[i]];
+    }
+    vector<tdd> sample = minijoin(S, R2_stratified);
+    
+    double estimate = 0.0;
+    
+    for(int i=0; i<m; i++) {
+        tdd t = sample[i];
+        double p_t = h1(get<0>(t), get<1>(t))*h2(get<2>(t))/normalisation;
+        estimate += aggregation_f(get<0>(t), get<1>(t), get<2>(t))/p_t;
+    }
+    
+    estimate /= (double) m;
+    return estimate;
 }
 
 
@@ -44,9 +93,9 @@ int main() {
     mtwist_seed(mt, 832982837UL);
 
     int m = 100;
-	double k_factor = 1.0;
-	double sigma = 0.99;
-	double sigma_factor = 1.0/log(1/sigma);
+    double k_factor = 1.0;
+    double sigma = 0.99;
+    double sigma_factor = 1.0/log(1/sigma);
 
     int n1          = 100;
     double skew1    = 1.0;
@@ -57,6 +106,29 @@ int main() {
     vector<pdd> R1 = zipvec(R1A, R1B);
     auto stratR1 = stratify(R1);
     
+    auto aggregate_f = [] (double A, double B, double C) -> double {return C;};
+    auto h1_unif = [] (double A, double B) -> double {return 1.0;};
+    auto h2_unif = [] (double C) -> double {return 1.0;};
+    auto h1_weighted = [] (double A, double B) -> double {return 1.0;};
+    auto h2_weighted = [] (double C) -> double {return C;};
+    auto exact_sampler = [] (int m, vector<double> w) -> vector<int> {
+                                return weighted_sample_indices(w.size(), get_cdf(w), m);
+                            };
+    auto heuristic_sampler = [] (int m, vector<double> w) -> vector<int> {
+                                double w_max = (*max_element(w.begin(), w.end()));
+                                double w_min = (*min_element(w.begin(), w.end()));
+                                double sigma = 0.99;//capture instead?
+                                double sigma_factor = 1.0/log(1.0/sigma);
+
+                                int k = m*m*w_max/w_min*sigma_factor;
+    
+                                vector<int> U = sample_indices(w.size(), k);
+                                vector<double> U_w(k);
+                                for(int i=0; i<k; i++) {
+                                    U_w[i] = w[U[i]];
+                                }
+                                return weighted_sample(U, get_cdf(U_w), m);
+                            };
 
     int n2          = 1000;
     double skew2    = 1.0;
@@ -65,7 +137,7 @@ int main() {
     vector<double> R2A = get_distribution(n2,skew2,ratio2,n_discrete2);
     vector<double> R2C = get_distribution(n2,1.0,n2);
     vector<pdd> R2 = zipvec(R2A, R2C);
-    auto stratR2 = stratify(R2);
+    Tstrat stratR2 = stratify(R2);
     auto R2stratcounts = stratify_counts(R2);
     
     auto J = join(stratR1, stratR2);
@@ -86,33 +158,43 @@ int main() {
     
     int nruns = 1;
     for(int run_i=0; run_i<nruns; run_i++) {
-	//SSJ
+    //SSJ
         vector<pdd> SSJ_S = weighted_sample(R1, SSJ_c_prob, m);
-		vector<tdd> SSJ_samp = minijoin(SSJ_S, stratR2);
-		double SSJ_estimate = 0.0;
-		for(auto t : SSJ_samp)
-			SSJ_estimate += aggregate_f(get<0>(t), get<1>(t), get<2>(t));
-		SSJ_estimate *= J.size()/(double)SSJ_samp.size();
-		cout << "SSJ   :" << SSJ_estimate << endl;
+        vector<tdd> SSJ_samp = minijoin(SSJ_S, stratR2);
+        double SSJ_estimate = 0.0;
+        for(auto t : SSJ_samp)
+            SSJ_estimate += aggregate_f(get<0>(t), get<1>(t), get<2>(t));
+        SSJ_estimate *= J.size()/(double)SSJ_samp.size();
+        cout << "SSJ   :" << SSJ_estimate << endl;
 
-	//HSSJ
-		int k = k_factor * m*m * sigma_factor
-				* (*max_element(SSJ_prob.begin(), SSJ_prob.end()))
-				/ (*min_element(SSJ_prob.begin(), SSJ_prob.end()));//HWS-heuristic
+    //HSSJ
+        int k = k_factor * m*m * sigma_factor
+                * (*max_element(SSJ_prob.begin(), SSJ_prob.end()))
+                / (*min_element(SSJ_prob.begin(), SSJ_prob.end()));//HWS-heuristic
         vector<int> HSSJ_U_indices = sample_indices(R1.size(), k);
-		vector<double> HSSJ_U_prob(k);
-		vector<pdd> HSSJ_U(k);
-		for(int i=0; i<k; i++) {
-			HSSJ_U_prob[i] = SSJ_prob[HSSJ_U_indices[i]];
-			HSSJ_U[i] = R1[HSSJ_U_indices[i]];
-		}
+        vector<double> HSSJ_U_prob(k);
+        vector<pdd> HSSJ_U(k);
+        for(int i=0; i<k; i++) {
+            HSSJ_U_prob[i] = SSJ_prob[HSSJ_U_indices[i]];
+            HSSJ_U[i] = R1[HSSJ_U_indices[i]];
+        }
         vector<pdd> HSSJ_S = weighted_sample(HSSJ_U, get_cdf(HSSJ_U_prob), m);
-		vector<tdd> HSSJ_samp = minijoin(HSSJ_S, stratR2);
-		double HSSJ_estimate = 0.0;
-		for(auto t : HSSJ_samp)
-			HSSJ_estimate += aggregate_f(get<0>(t), get<1>(t), get<2>(t));
-		HSSJ_estimate *= J.size()/(double)HSSJ_samp.size();
-		cout << "HSSJ  :" << HSSJ_estimate << " (k = " << k << ")" << endl;
+        vector<tdd> HSSJ_samp = minijoin(HSSJ_S, stratR2);
+        double HSSJ_estimate = 0.0;
+        for(auto t : HSSJ_samp)
+            HSSJ_estimate += aggregate_f(get<0>(t), get<1>(t), get<2>(t));
+        HSSJ_estimate *= J.size()/(double)HSSJ_samp.size();
+        cout << "HSSJ  :" << HSSJ_estimate << " (k = " << k << ")" << endl;
+    //Compact alternatives
+        cout << "SSJ_alt:" << 
+            generic_sample_join(h1_unif, h2_unif, m, R1, R2, exact_sampler, aggregate_f) << endl;
+        cout << "HSSJ_alt:" << 
+            generic_sample_join(h1_unif, h2_unif, m, R1, R2, heuristic_sampler, aggregate_f) << endl;
+        cout << "WS_join:" <<
+                generic_sample_join(h1_weighted, h2_weighted, m, R1, R2, exact_sampler, aggregate_f) << endl;
+        cout << "HWS_join:" <<
+                generic_sample_join(h1_weighted, h2_weighted, m, R1, R2, heuristic_sampler, aggregate_f) << endl;
+        
     }
     
     return 0;
--- quality_comparison/sampleJoins.h
+++ quality_comparison/sampleJoins.h
@@ -20,9 +20,9 @@ mtwist* mt;
 //output: corresponding CDF (from w[0]/W to 1)
 vector<double> get_cdf(const vector<double>& w) {
     vector<double> result(w.size());
-	result[0]=w[0];
+    result[0]=w[0];
     for(int i=1; i<w.size(); i++) //set result to cumsum of w
-		result[i] = result[i-1]+w[i];
+        result[i] = result[i-1]+w[i];
     for(int i=0; i<w.size(); i++) //normalize result (last element should be 1)
         result[i]/=result[w.size()-1];
     return result;
@@ -31,187 +31,187 @@ vector<double> get_cdf(const vector<double>& w) {
 //input:  two columns of data
 //output: data stratified by first column
 Tstrat stratify(const vector<pdd>& R) {
-	Tstrat result;
-	for(vector<pdd>::const_iterator Rit = R.begin(); Rit!=R.end(); ++Rit) {
-		if(result.find(Rit->first) == result.end()) {
-			result.insert(make_pair(Rit->first,vector<pdd>()));//for first element in strata, insert empty vector
-		}
-		result[Rit->first].push_back(*Rit);
-	}
-	return result;
+    Tstrat result;
+    for(vector<pdd>::const_iterator Rit = R.begin(); Rit!=R.end(); ++Rit) {
+        if(result.find(Rit->first) == result.end()) {
+            result.insert(make_pair(Rit->first,vector<pdd>()));//for first element in strata, insert empty vector
+        }
+        result[Rit->first].push_back(*Rit);
+    }
+    return result;
 }
 
 //input:  two columns of data
 //output: map from first column to size of strata
 map<double, int> stratify_counts(const vector<pdd>& R) {
-	Tstrat Rstrat = stratify(R);
-	
-	map<double, int> result;
-	for(Tstrat::const_iterator Rit = Rstrat.begin(); Rit!=Rstrat.end(); ++Rit) {
-		result[Rit->first]=(Rit->second).size();
-	}
-	return result;
+    Tstrat Rstrat = stratify(R);
+    
+    map<double, int> result;
+    for(Tstrat::const_iterator Rit = Rstrat.begin(); Rit!=Rstrat.end(); ++Rit) {
+        result[Rit->first]=(Rit->second).size();
+    }
+    return result;
 }
 
 map<double, int> strat_count_multiply(const map<double, int>& a, const map<double, int>& b) {
-	map<double, int> result;
-	for(map<double, int>::const_iterator ait = a.begin(); ait != a.end(); ++ait) {
-		map<double, int>::const_iterator bit = b.find(ait->first);
-		if(bit != b.end()) {
-			result.insert(make_pair(ait->first, ait->second*bit->second));
-		}
-	}
-	return result;
+    map<double, int> result;
+    for(map<double, int>::const_iterator ait = a.begin(); ait != a.end(); ++ait) {
+        map<double, int>::const_iterator bit = b.find(ait->first);
+        if(bit != b.end()) {
+            result.insert(make_pair(ait->first, ait->second*bit->second));
+        }
+    }
+    return result;
 }
 
 //Obtain sample with replacement of size k
 template <typename T> vector<T> sample(const vector<T>& R, int k) {
-	vector<T> result(k);
-	int n = R.size();
-	for(int i=0; i<k; i++) {
-		result[i]=R[mtwist_uniform_int(mt,0,n-1)];
-	}
-	return result;
+    vector<T> result(k);
+    int n = R.size();
+    for(int i=0; i<k; i++) {
+        result[i]=R[mtwist_uniform_int(mt,0,n-1)];
+    }
+    return result;
 }
 
 //Obtain sample with replacement of size k from a list of indices {0,1,...,n-2,n-1}
 vector<int> sample_indices(int n, int k) {
-	vector<int> result(k);
-	for(int i=0; i<k; i++) {
-		result[i]=mtwist_uniform_int(mt,0,n-1);
-	}
-	return result;
+    vector<int> result(k);
+    for(int i=0; i<k; i++) {
+        result[i]=mtwist_uniform_int(mt,0,n-1);
+    }
+    return result;
 }
 
 //input:   R   - population to sample from
-//		   c_p - normalized cumilative probability distribution 
-//		   m   - sample size
+//           c_p - normalized cumilative probability distribution 
+//           m   - sample size
 //output:  weighted sample with replacement of size m
 template <typename T> vector<T> weighted_sample(const vector<T>& R, const vector<double>& c_p, int m) {
-	vector<T> result(m);
-	int n = R.size();
-	for(int i=0; i<m; i++) {
-		double random_variate = mtwist_drand(mt);
-		vector<double>::const_iterator c_p_it = upper_bound(c_p.begin(), c_p.end(), random_variate);
-		int index = c_p_it-c_p.begin();
-		result[i]=R[index];
-	}
-	return result;
+    vector<T> result(m);
+    int n = R.size();
+    for(int i=0; i<m; i++) {
+        double random_variate = mtwist_drand(mt);
+        vector<double>::const_iterator c_p_it = upper_bound(c_p.begin(), c_p.end(), random_variate);
+        int index = c_p_it-c_p.begin();
+        result[i]=R[index];
+    }
+    return result;
 }
 
 //same as weighted_sample, but R is replaced by {0, 1, ..., n-2, n-1}
 vector<int> weighted_sample_indices(int n, const vector<double>& c_p, int m) {
-	vector<int> result(m);
-	for(int i=0; i<m; i++) {
-		double random_variate = mtwist_drand(mt);
-		vector<double>::const_iterator c_p_it = upper_bound(c_p.begin(), c_p.end(), random_variate);
-		int index = c_p_it-c_p.begin();
-		result[i]=index;
-	}
-	return result;
+    vector<int> result(m);
+    for(int i=0; i<m; i++) {
+        double random_variate = mtwist_drand(mt);
+        vector<double>::const_iterator c_p_it = upper_bound(c_p.begin(), c_p.end(), random_variate);
+        int index = c_p_it-c_p.begin();
+        result[i]=index;
+    }
+    return result;
 }
 
 
 template <typename T> vector<T> approximate_weighted_sample(const vector<T>& R, const vector<double>& c_w,
-															int m, double w_ratio, double delta) {
-	//NOTE: we assume c_w is cumilative weight distribution from 0 to 1!
-	//delta is the desired maximum normalised absolute difference between the projected total weight from U and the total weight in R
-	int min_k = ceil(w_ratio*m*m);
+                                                            int m, double w_ratio, double delta) {
+    //NOTE: we assume c_w is cumilative weight distribution from 0 to 1!
+    //delta is the desired maximum normalised absolute difference between the projected total weight from U and the total weight in R
+    int min_k = ceil(w_ratio*m*m);
 
-	if(min_k > R.size()) {//NOTE: in practice we may want to switch from AWS to WS at much lower sampling fraction, depending on runtime cutoff.
-						  //This switch is just here to avoid run-away runtimes, and will not influence determination of crossover since it is much beyond the crossover point
-		cout << ("WARNING: oversampling in AWS! Early switch to regular sampling.\n");
-		cout << ("AWS effective sampling fraction 1.0\n");
-		return(weighted_sample(R, c_w, m));
-	}
+    if(min_k > R.size()) {//NOTE: in practice we may want to switch from AWS to WS at much lower sampling fraction, depending on runtime cutoff.
+                          //This switch is just here to avoid run-away runtimes, and will not influence determination of crossover since it is much beyond the crossover point
+        cout << ("WARNING: oversampling in AWS! Early switch to regular sampling.\n");
+        cout << ("AWS effective sampling fraction 1.0\n");
+        return(weighted_sample(R, c_w, m));
+    }
 
-	double memory_factor = 2.0;
-	double U_weight = 0;
+    double memory_factor = 2.0;
+    double U_weight = 0;
 
-	vector<double> w_U;
-	vector<int> U_indices;
-	w_U.reserve(ceil(memory_factor*min_k));
-	U_indices.reserve(ceil(memory_factor*min_k));
+    vector<double> w_U;
+    vector<int> U_indices;
+    w_U.reserve(ceil(memory_factor*min_k));
+    U_indices.reserve(ceil(memory_factor*min_k));
 
-	int n=R.size();
-	int k;
-	for(k=0; k<min_k || abs(U_weight*n/(double)k-1.0) > delta; k++) {
-						//Note; U_weight*n/(double)k is the projected total weight from U
-		//int j = *sample_indices(n,1).begin();
-		int j=mtwist_uniform_int(mt,0,n-1);
-		U_indices.push_back(j);
+    int n=R.size();
+    int k;
+    for(k=0; k<min_k || abs(U_weight*n/(double)k-1.0) > delta; k++) {
+                        //Note; U_weight*n/(double)k is the projected total weight from U
+        //int j = *sample_indices(n,1).begin();
+        int j=mtwist_uniform_int(mt,0,n-1);
+        U_indices.push_back(j);
 
-		if(j == 0)
-			w_U.push_back(c_w[j]);
-		else
-			w_U.push_back(c_w[j] - c_w[j-1]);
+        if(j == 0)
+            w_U.push_back(c_w[j]);
+        else
+            w_U.push_back(c_w[j] - c_w[j-1]);
 
-		U_weight += w_U[k];
-		if(k > R.size()) {
-			cout << ("WARNING: oversampling in AWS! Late switch to regular sampling.\n");
-			cout << ("AWS effective sampling fraction 1.0\n");
-			return(weighted_sample(R, c_w, m));
-		}
-	}
-	if(k > min_k) {//Only print effective sampling fraction if it is not min_k/n1
-		cout << "AWS effective sampling fraction " << k/(double)R.size() << " (default sampling fraction is " << min_k/(double)R.size() << ")" << endl;
-	}
+        U_weight += w_U[k];
+        if(k > R.size()) {
+            cout << ("WARNING: oversampling in AWS! Late switch to regular sampling.\n");
+            cout << ("AWS effective sampling fraction 1.0\n");
+            return(weighted_sample(R, c_w, m));
+        }
+    }
+    if(k > min_k) {//Only print effective sampling fraction if it is not min_k/n1
+        cout << "AWS effective sampling fraction " << k/(double)R.size() << " (default sampling fraction is " << min_k/(double)R.size() << ")" << endl;
+    }
 
-	vector<double> c_w_U = get_cdf(w_U);
-	
-	vector<int> S_indices = weighted_sample(U_indices, c_w_U, m);
-	vector<T> S(m);
-	for(int i=0; i<m; i++) {
-		S[i] = R[S_indices[i]];
-	}
-	return S;
+    vector<double> c_w_U = get_cdf(w_U);
+    
+    vector<int> S_indices = weighted_sample(U_indices, c_w_U, m);
+    vector<T> S(m);
+    for(int i=0; i<m; i++) {
+        S[i] = R[S_indices[i]];
+    }
+    return S;
 }
 
 
 vector<pdd> zipvec(const vector<double>& l, const vector<double>& r) {
-	assert(l.size() == r.size());
-	int n=l.size();
-	vector<pdd> result(n);
-	for(int i=0; i<n; i++) {
-		result[i] = make_pair(l[i],r[i]);
-	}
-	return result;
+    assert(l.size() == r.size());
+    int n=l.size();
+    vector<pdd> result(n);
+    for(int i=0; i<n; i++) {
+        result[i] = make_pair(l[i],r[i]);
+    }
+    return result;
 }
 
 //Join two 2-column relations on the first attribute to form a 3-column relation
 vector<tdd> join(const Tstrat& R1, const Tstrat& R2) {
-	vector<tdd> result;
-	for(auto strat1 : R1) {
-		double a = strat1.first;
-		auto strat2it = R2.find(a);
-		if(strat2it == R2.end())
-			continue; //key does not join
-		for(auto t1 : strat1.second)
-		for(auto t2 : strat2it->second) {
-			result.push_back(make_tuple(t1.first, t1.second, t2.second));
-		}
-	}
-	return result;
+    vector<tdd> result;
+    for(auto strat1 : R1) {
+        double a = strat1.first;
+        auto strat2it = R2.find(a);
+        if(strat2it == R2.end())
+            continue; //key does not join
+        for(auto t1 : strat1.second)
+        for(auto t2 : strat2it->second) {
+            result.push_back(make_tuple(t1.first, t1.second, t2.second));
+        }
+    }
+    return result;
 }
 
 vector<pdd> printvec(const vector<pdd>& V) {
-	for(auto v : V) {
-		cout << v.first << "\t" << v.second << endl;
-	}
-	cout << endl;
+    for(auto v : V) {
+        cout << v.first << "\t" << v.second << endl;
+    }
+    cout << endl;
 }
 
 
 vector<tdd> minijoin(const vector<pdd>& S, const Tstrat& R2) {
-	vector<tdd> result;
-	for(auto t1 : S) {
-		auto strat2it = R2.find(t1.first);
-		if(strat2it == R2.end())
-			continue; //key does not join
-		auto t2 = sample(strat2it->second,1)[0];
-		result.push_back(make_tuple(t1.first, t1.second, t2.second));
-	}
-	return result;
+    vector<tdd> result;
+    for(auto t1 : S) {
+        auto strat2it = R2.find(t1.first);
+        if(strat2it == R2.end())
+            continue; //key does not join
+        auto t2 = sample(strat2it->second,1)[0];
+        result.push_back(make_tuple(t1.first, t1.second, t2.second));
+    }
+    return result;
 }
 
 
